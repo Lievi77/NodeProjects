f8fcff7bead708791f9799ce38e8a4e8f42cb3df
Lievi77 <lev.guzman@ideen.mx>
2021-01-06 16:54:24 -0500

    Formatted code for node-basis projects

--- node-basis/app.js
+++ node-basis/app.js
@@ -2,36 +2,33 @@
 // email: levcguzman77@gmail.com
 
 /*
-* A simple app that let us display multiplicative tables 
-*/
+ * A simple app that let us display multiplicative tables
+ */
 
-
-const argv = require('./config/yargs').argv; //handles CLI inputs way better
-const colors = require('colors/safe'); //allows to display CLI text with colors
+const argv = require("./config/yargs").argv; //handles CLI inputs way better
+const colors = require("colors/safe"); //allows to display CLI text with colors
 
 //an example of deconstruction, we only extract the methods we want
-const { createFile, listTable } = require('./multiplication/multiply');
+const { createFile, listTable } = require("./multiplication/multiply");
 
 //argv is in the form of an object, the commands are located in argv[_] as an array.
-// Example: ["create", "generate", ...] 
+// Example: ["create", "generate", ...]
 let command = argv._[0]; //we are only interested in the first argument
 
-
 switch (command) {
+  case "list":
+    //argument values are located in argv[argument_name]
+    //usage: node app list --base=5 --limit=20
+    listTable(argv.base, argv.limit);
+    break;
 
-    case 'list':
-        //argument values are located in argv[argument_name]
-        //usage: node app list --base=5 --limit=20
-        listTable(argv.base, argv.limit);
-        break;
+  case "create":
+    createFile(argv.base, argv.limit) // returns a promise, followup with a .then
+      .then((file) => console.log(`File Created: `, colors.green(file)))
+      .catch((e) => console.log(e));
+    break;
 
-    case 'create':
-        createFile(argv.base, argv.limit) // returns a promise, followup with a .then
-            .then(file => console.log(`File Created: `, colors.green(file)))
-            .catch(e => console.log(e));
-        break;
-
-    default: //executes if everything else fails
-        console.log('Unsupported command');
-
+  default:
+    //executes if everything else fails
+    console.log("Unsupported command");
 }
--- node-basis/config/yargs.js
+++ node-basis/config/yargs.js
@@ -1,25 +1,25 @@
-// we will write here the yargs import to simplify code 
+// we will write here the yargs import to simplify code
 
-//command options 
+//command options
 const opts = {
-    base: { //the basis of our multiplicative table
-        demand: true, //makes the argument required
-        alias: 'b' //alias for simplification
-    },
-    limit: { //upper limit
-        alias: 'l',
-        default: 10
-    }
-}
+  base: {
+    //the basis of our multiplicative table
+    demand: true, //makes the argument required
+    alias: "b", //alias for simplification
+  },
+  limit: {
+    //upper limit
+    alias: "l",
+    default: 10,
+  },
+};
 
+const argv = require("yargs")
+  .command("list", "Prints the table on screen", opts) //creates a command and displays its description when --help is called
+  .command("create", "Generates a .txt file containing the table", opts)
+  .help().argv; //must be called in order to display the description of each method
 
-const argv = require('yargs')
-    .command('list', 'Prints the table on screen', opts) //creates a command and displays its description when --help is called
-    .command('create', 'Generates a .txt file containing the table', opts)
-    .help() //must be called in order to display the description of each method
-    .argv;
-
-    //export command
+//export command
 module.exports = {
-    argv
-}
+  argv,
+};
