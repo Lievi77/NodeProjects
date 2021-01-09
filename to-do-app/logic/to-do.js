903ccc939d7bbbdebdd10482958021f82aa2423d
Lievi77 <lev.guzman@ideen.mx>
2021-01-09 10:53:44 -0500

    Implemented loadDB

--- to-do-app/logic/to-do.js
+++ to-do-app/logic/to-do.js
@@ -8,6 +8,8 @@ Creates a new to-do task with a description
 @param description The description of the task
 */
 const create = (description) => {
+  loadDb();
+
   let todo = {
     description,
     completed: false, //default value of a new todo is always false
@@ -16,7 +18,7 @@ const create = (description) => {
 
   saveToDB();
 
-  //return todo to signalized that the task was completed successfully
+  //return to-do to signalize that the task was created successfully
   return todo;
 };
 
@@ -29,6 +31,20 @@ const saveToDB = () => {
   fs.writeFile("database/data.json", data, (err) => {
     if (err) throw new Error("Failed to write to database", err);
   }); //writes the file
+};
+
+/*
+Reads our db 
+*/
+const loadDb = () => {
+  try {
+    listToDos = require("../database/data.json"); //automatically parses the JSON for us
+  } catch (error) {
+    listToDos = []; //file is empty case
+  }
+
+  //debug
+  //console.log("loaded successfully:", listToDos);
 };
 
 module.exports = {
