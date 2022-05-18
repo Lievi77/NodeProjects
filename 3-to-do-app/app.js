40ba55c286af2e4fd5a20a8dfe46207490c99461
Lev <lguzm038@uottawa.ca>
2022-05-18 17:54:43 -0500

    implement option 1 and 2 logic

--- 3-to-do-app/app.js
+++ 3-to-do-app/app.js
@@ -1,14 +1,32 @@
 require("colors");
-const { inquirerMenu, pause } = require("./helpers/inquirer");
+const { inquirerMenu, pause, readInput } = require("./helpers/inquirer");
+const Todo = require("./models/todo");
+const Todos = require("./models/todos");
 
 const main = async () => {
 
     let opt = '';
+    const todos = new Todos();
 
     do {
 
         opt = await inquirerMenu();
         console.log({ opt });
+
+        switch (opt) {
+            case '1':
+                //create task
+                //request input via require
+                const desc = await readInput('Enter a description:');
+                todos.createTodo(desc);
+                break;
+
+            case '2':
+                console.log(todos._list);
+                break;
+            default:
+                break;
+        }
 
         await pause();
 
