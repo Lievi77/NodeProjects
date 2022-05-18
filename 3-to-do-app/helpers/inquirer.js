773af79ff042666eb592cda34a9b51e481154fb1
Lev <lguzm038@uottawa.ca>
2022-05-18 18:02:55 -0500

    Aesthetic changes to display menu

--- 3-to-do-app/helpers/inquirer.js
+++ 3-to-do-app/helpers/inquirer.js
@@ -12,31 +12,31 @@ const questions = [
 
             {
                 value: "1",
-                name: "1. Create a TO-DO"
+                name: `${'1'.green}. Create a TO-DO`
             },
             {
                 value: "2",
-                name: "2. List TO-DOs"
+                name: `${'2'.green}. List TO-DOs`
             },
             {
                 value: "3",
-                name: "3. List completed TO-DOs"
+                name: `${'3'.green}. List completed TO-DOs`
             },
             {
                 value: "4",
-                name: "4. List pending TO-DOs"
+                name: `${'4'.green}. List pending TO-DOs`
             },
             {
                 value: "5",
-                name: "5. Complete a TO-DO(s)"
+                name: `${'5'.green}. Complete a TO-DO(s)`
             },
             {
                 value: "6",
-                name: "6. Delete a TO-DO"
+                name: `${'6'.green}. Delete a TO-DO`
             },
             {
                 value: "0",
-                name: "0. Exit"
+                name: `${'0'.green}. Exit`
             },
         ]
     }
@@ -46,7 +46,7 @@ const inquirerMenu = async () => {
     console.clear();
 
     console.log('============================'.green);
-    console.log("      Select an option:".green);
+    console.log("      Select an option:".white);
     console.log('============================\n'.green);
 
     const { option } = await inquirer.prompt(questions);
